4f8402e20defcf48d2acf6d8ff3c146825544ce2
Bindu818 <bindudoddapaneni818@gmail.com>
2025-04-09 09:50:56 -0400

    Update Calculator.java

---
+++ src/main/java/in/javahome/myweb/controller/Calculator.java
@@ -0,0 +1,22 @@
+package in.javahome.myweb.controller;
+/*
+ * 
+ */
+public class Calculator {
+	
+	/**
+	 * @param i First number
+	 * @param j Second number
+	 * @return int
+	 */
+	public int add(int i, int j){
+		return i+j;
+	}
+	public int multiply(int i, int j){
+		return i*j;
+	}
+	
+	public int sub(int i, int j){
+		return i-j;
+	}
+}
